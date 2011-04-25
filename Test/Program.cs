a16819ddd1ce59bc19db801188dfa829cc4af35d
Joe ST <joe@fbstj.net>
2011-04-25 20:34:25 +0100

    Make sure you can actually read the files.

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace fbstj.dbpf.tests
+﻿using System;
+using System.IO;
+
+namespace fbstj.dbpf.tests
 {
     class Program
     {
+        const string TEST_FILE = "../../NetworkAddonMod_IndividualNetworkRULs.dat";
         static void Main(string[] args)
         {
+            FileStream fs = File.Open(TEST_FILE, FileMode.Open);
+            byte[] buf = new byte[4];
+            fs.Seek(0, SeekOrigin.Begin);
+            fs.Read(buf, 0, 4);
+            foreach(byte b in buf)
+                Console.Write((char)b);
+            while (true) ;//keep open
         }
     }
 }
