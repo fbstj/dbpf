7543dd647b226b2b08caca16967cd4cb9a7afe97
Joe ST <joe@fbstj.net>
2012-01-20 19:48:30 +0000

    [dbpf] move to Node Buffers rather than ArrayBuffers  - fix bug in Header  - fix M and m functions in Header.serialize  - add word function to abstract away from buf object

--- dbpf.js
+++ dbpf.js
@@ -1,48 +1,46 @@
-require('DataView.extensions')
 
 function DBPF_Header(){
 	this.Version = 1.0
 	this.UserVersion = 0.0
 	this.Flags = 0; this.Ctime = 0; this.Atime = 0
-	this.Index = { Version: 7.0, Count: 0, Offest: 0, Size: 0 }
-	this.Holes = { Count: 0, Offest: 0, Size: 0 }
+	this.Index = { Version: 7.0, Count: 0, Offset: 0, Size: 0 }
+	this.Holes = { Count: 0, Offset: 0, Size: 0 }
 }
+
 DBPF_Header.prototype = {
 	serialize: function()
 	{	// serializes this into a 96 byte ArrayBuffer
-		var M = function(v){ return v.toString().split('.')[0] },
-			m = function(v){ return v.toString().split('.')[1] }, 
-			buf = new ArrayBuffer(96), dv = new DataView(buf)
-		dv.setString(0, "DBPF")
-		dv._splice('u32', 1, [
-			M(this.Version), m(this.Version),
-			M(this.UserVersion), m(this.UserVersion),
-			this.Flags, this.Ctime, this.Atime,
-			M(this.Index.Version),
-			this.Index.Count, this.Index.Offset, this.Index.Size,
-			this.Holes.Count, this.Holes.Offset, this.Holes.Size,
-			m(this.Index.Version),
-			this.Index.Offset
-		])
+		var M = function(v){ return Number(String(v).split('.')[0] || 0) },
+			m = function(v){ return Number(String(v).split('.')[1] || 0) }, 
+			buf = new Buffer(96), i = 0,
+			word = function(v){ buf.writeUInt32BE(v, 4*i++) }
+		buf.write("DBPF", 0, 4*i++, 'ascii')
+		word(M(this.Version)); word(m(this.Version))
+		word(M(this.UserVersion)); word(m(this.UserVersion))
+		word(this.Flags); word(this.Ctime); word(this.Atime)
+		word(M(this.Index.Version))
+		word(this.Index.Count); word(this.Index.Offset); word(this.Index.Size)
+		word(this.Holes.Count); word(this.Holes.Offset); word(this.Holes.Size)
+		word(m(this.Index.Version))
+		word(this.Index.Offset)
 		return buf
 	},
 	deserialize: function(buf)
 	{	// deserializes an ArrayBuffer into this
 		var version = function(M, m) { return Number(M + '.' + m) },
-			dv = new DataView(buf)
-			words = dv._slice('u32', 0, 16)
-		if(dv.getString(0, 4) != "DBPF")
+			word = function(i){ return buf.readUInt32BE(4*i) }
+		if (buf.toString('ascii',0, 4) != "DBPF")
 			throw new TypeError("not a DBPF buffer")
-		this.Version = version(words[1],words[2])
-		this.UserVersion = version(words[3], words[4])
-		this.Flags = words[5]; this.Ctime = words[6]; this.Atime =words[7]
-		this.Index.Version = version(words[8], words[15])
-		this.Index.Count = words[9]
-		this.Index.Offset = words[(words[15] == 3)?16:10]
-		this.Index.Size = words[11]
-		this.Holes.Count = words[12]
-		this.Holes.Offset = words[13]
-		this.Holes.Size = words[14]
+		this.Version = version(word(1),word(2))
+		this.UserVersion = version(word(3), word(4))
+		this.Flags = word(5); this.Ctime = word(6); this.Atime = word(7)
+		this.Index.Version = version(word(8), word(15))
+		this.Index.Count = word(9)
+		this.Index.Offset = word((word(15) == 3)?16:10)
+		this.Index.Size = word(11)
+		this.Holes.Count = word(12)
+		this.Holes.Offset = word(13)
+		this.Holes.Size = word(14)
 	},
 }
 
