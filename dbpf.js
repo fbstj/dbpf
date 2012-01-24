0bf6ceb6d7d7efa73ad686b98612cbf33261835b
Joe ST <joe@fbstj.net>
2012-01-24 21:39:43 +0000

    [dbpf] abstract out word functions to word.R and .W  - bind/apply/call to suit  - move Header.Load to an array of words  - Header.Save calls rahter than binds  - Index() is empty  - I_meta and I_record helpers structs  - Index.Load and Index.Save work [test] test loding and saving  - load existing DBPF files  - save files created by dbpf.js  - load files created by dbpf.js

--- dbpf.js
+++ dbpf.js
@@ -6,6 +6,11 @@ function map(cb){	// iterates over this, calling cb(key,value)
 	for(var i in this)	cb(i, this[i])
 }
 
+var word = {
+	R: function(i, o){ return this.readUInt32LE(Number(i)*4+(o||0)) },
+	W: function(i, v, o){ this.writeUInt32LE(v, Number(i)*4+(o||0)) },
+}
+
 function DBPF_Header(V, uV, F, C, A, iV, iC, iO, iS, hC, hO, hS){
 	this.Version = V || 1.0
 	this.UserVersion = uV || 0.0
@@ -13,41 +18,72 @@ function DBPF_Header(V, uV, F, C, A, iV, iC, iO, iS, hC, hO, hS){
 	this.Index = { Version: iV||7.0, Count: iC||0, Offset: iO||0, Size: iS||0 }
 	this.Holes = { Count: hC||0, Offset: hO||0, Size: hS||0 }
 }
-exports.Header = function(){ var o = {}; DBPF_Header.apply(o, arguments); return o }
+exports.Header=function(){var o={};DBPF_Header.apply(o,arguments);return o}
 
 // bind to a buffer
 exports.Header.Load = function(){
-var _ = this.slice(0, 96)
-function W(i){ return _.readUInt32LE(i*4) }
+var _ = this.slice(0, 96), W = []
 	if (_.toString('ascii',0, 4) != "DBPF")
 		throw new TypeError("not a DBPF buffer")
-
-	return new DBPF_Header( V(W(1), W(2)), V(W(3), W(4)), W(5), W(6), W(7),
-							V(W(8), W(15)), W(9), W((W(15) == 3)?16:10), W(11),
-							W(12), W(13), W(14) );
+	for(var i = 0; i < 17; i++)
+		W.push(word.R.call(_, i))
+	return new DBPF_Header( V(W[1], W[2]), V(W[3], W[4]), W[5], W[6], W[7],
+							V(W[8], W[15]), W[9], W[(W[15] == 3)?16:10], W[11],
+							W[12], W[13], W[14] );
 }
 exports.Header.Save = function(h){
 var _ = this.slice(0, 96)
+	_.fill(0)
 	_.write("DBPF", 0, 4, 'ascii')
-	map.bind([
+	map.call([
 		V.M(h.Version), V.m(h.Version),
 		V.M(h.UserVersion), V.m(h.UserVersion), h.Flags, h.Ctime, h.Atime,
 		V.M(h.Index.Version), h.Index.Count, h.Index.Offset, h.Index.Size,
 		h.Holes.Count, h.Holes.Offset, h.Holes.Size,
 		V.m(h.Index.Version), h.Index.Offset
-	])(function(i, v){ _.writeUInt32LE(v, 4*(Number(i)+1)) })
+	], function(i, v){ word.W.call(_, Number(i)+1, v) })
 }
 
-function TGI(t,g,i){
-	return { Type: t, Group: g, Instance: i }
+exports.Index = function(){}
+
+function I_meta(h){
+	return { C: h.Index.Count, O: h.Index.Offset, S: h.Index.Size,
+		get W(){ return this.S/this.C/4 },
+	}
+}
+function I_record(){
+	return { 
+		Type: Number(this.shift()) || 0,
+		Group: Number(this.shift()) || 0,
+		Instance: Number(this.shift()) || 0,
+
+		Size: Number(this.pop()) || 0,
+		Offset: Number(this.pop()) || 0,
+	}
 }
 
-exports.Index = {}
-
-exports.Index.Load = function(h){
-var i = [], b = this.slice(h.Index.Offset,h.Index.Size)
+exports.Index.Load = function(h){ var H = I_meta(h)
+var i = [], _ = this.slice(H.O), W = word.R.bind(_)
+	for(var j = 0; j < H.C; j++)
+	{
+		var a = []
+		for(var k = 0; k < H.W; k++)
+			a.push(W(j * H.W + k))
+		i.push(I_record.call(a))
+	}
 	return i
 }
-exports.Index.Save = function(h, i){
-	;
+exports.Index.Save = function(h, i){ var H = I_meta(h)
+var _ = this.slice(H.O), W = word.W.bind(_)
+	map.call(i, function(k, v){
+		var j = Number(k)*H.W
+		console.log((j+5)*4)
+		W(j + 0, v.Type)
+		W(j + 1, v.Group)
+		W(j + 2, v.Instance)
+		W(j + 3, v.Size)
+		W(j + 4, v.Offset)
+	})
 }
+
+exports.File = function(i){ return this.slice(i.Offset, i.Offset+i.Size) }
--- test.js
+++ test.js
@@ -1,20 +1,45 @@
 var dbpf = require('dbpf'), fs = require('fs')
 
 function test_headers(){
+console.log('#test headers')
 var h = dbpf.Header(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 console.log(h)
 var b = new Buffer(96)
+b.fill(0)
 console.log(b)
 dbpf.Header.Save.call(b, h)
 console.log(b)
 var H = dbpf.Header.Load.call(b)
 console.log(H)
-};// test_headers()
+}; test_headers()
 
 function test_file(){
+console.log('#test file')
+// load existing DBPF
 var f = fs.readFileSync('../DJEM.dat')
+// load header
 var h = dbpf.Header.Load.call(f)
 console.log(h)
-
+// load index
+var i = dbpf.Index.Load.call(f, h)
+console.log(i)
+// create new DBPF
+var b = new Buffer(f.length)
+b.fill(0)
+// save Header
+dbpf.Header.Save.call(b, h)
+// save Index
+dbpf.Index.Save.call(b, h, i)
+// save File
+fs.writeFileSync('DJEM.dat', b)
+console.log('#test save')
+// reload file
+var f = fs.readFileSync('DJEM.dat')
+// load header
+var h = dbpf.Header.Load.call(f)
+console.log(h)
+// load index
+var i = dbpf.Index.Load.call(f, h)
+console.log(i)
 }; test_file()
 
