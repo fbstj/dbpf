6dc16ee14a94a751fa5ef2e1dc1a71c1e9db4a9b
Joe ST <joe@fbstj.net>
2013-03-06 21:50:27 +0000

    [classes] refactord - TGI is a lazy type for help with searching - Record uses __cmp__ to compare to TGIs - DBPF.header is now array - split record and dir searches - dir search uses DBPF.search

--- dbpf.py
+++ dbpf.py
@@ -4,28 +4,48 @@ from collections import namedtuple
 
 DIR = int('E86B1EEF',16)
 
-def match_parts(parts, a, b):
-	if not isinstance(a, dict) or not isinstance(b, dict):
-		raise Exception('arguments incorrect')
-	for p in parts:
-		if a[p] != b[p]:
-			return False
-	return True
+class TGI:
+	def __init__(self, tid=None, gid=None, iid=None):
+		self.type = tid
+		self.group = gid
+		self.instance = iid
+	@staticmethod
+	def __fmt(value=None):
+		return "?" if value is None else "{:X}".format(value)
 
-class Header(namedtuple('DBPF_header',[ 'magic',
-			'version_major', 'version_minor',
-			'user_version_major', 'user_version_minor',
-			'flags', 'ctime', 'mtime',
-			'index_version_major',
-			'index_count', 'index_offset', 'index_size',
-			'holes_count', 'holes_offset', 'holes_size',
-			'index_version_minor', 'index_offset_v2',
-			'unknown', 'reserved',
-		])):
-	bytes = struct.Struct("4s17i24s")
+	def __repr__(self):
+		return "T{:s}:G{:s}:I{:s}".format(
+					self.__fmt(self.type),
+					self.__fmt(self.group),
+					self.__fmt(self.instance)
+				)
 
-class Index(namedtuple("DBPF_Index", 'version count offset size')):
-	pass
+class Index(namedtuple("DBPF_Index", 'version count offset size')): pass
+class Record:
+	def __init__(self, tid, gid, iid, offset, raw=None, size=None):
+		self.type = tid
+		self.group = gid
+		self.instance = iid
+		self.offset = offset
+		self.raw = raw
+		self.size = size
+
+	def __cmp__(self, to):
+		if not isinstance(to, TGI):
+			return 1;
+		if to.type is not None:
+			if self.type != to.type:
+				return 2
+		if to.group is not None:
+			if self.group != to.group:
+				return 3
+		if to.instance is not None:
+			if self.instance != to.instance:
+				return 4
+		print self, "==", to
+		return 0
+	def __repr__(self):
+		return "T{:X}:G{:X}:I{:X}".format(self.type,self.group,self.instance)
 
 class DBPF:
 	@property
@@ -50,65 +70,65 @@ class DBPF:
 			raise ArgumentException('File')
 		self._fd = fd;
 		fd.seek(0)
-		_h = Header.bytes
-		h = self.header = Header._make(_h.unpack(fd.read(_h.size)))
-		if h.magic != b'DBPF':
+		hb = struct.Struct("4s17i24s")
+		self.header = hb.unpack(fd.read(hb.size))
+		if self.header[0] != b'DBPF':
 			raise DBPFException('Not a DBPF file')
-		self.records = self._scan_records()
+		self.records = []
+		self._scan_records()
+		for f in self.records
+			print f
+		self._scan_dir()
 
 	def _record_bytes(self):
 		return struct.Struct({7.0:'5I', 7.1:'6I'}.get(self.index.version, ''))
 
 	def _scan_records(self):
-		files = []
-		ind = self.index
 		# read index
-		for id in range(ind.count):
-			rb = self._record_bytes()
-			self._fd.seek(ind.offset + rb.size * id)
-			x = rb.unpack(self._fd.read(rb.size))
-			y = dict(
-				type = x[0], group = x[1], instance = x[2],
-				offset = x[-2],
-			)
-			self._fd.seek(y['offset'])
-			y['raw'] = self._fd.read(x[-1])
-			files.append(y)
+		rb = self._record_bytes()
+		o = self.index.offset
+		seek = self._fd.seek
+		read = self._fd.read
+		for id in range(self.index.count):
+			seek(o + rb.size * id)
+			x = rb.unpack(read(rb.size))
+			seek(x[-2])
+			self.records.append(Record(
+				tid = x[0], gid = x[1], iid = x[2],
+				offset = x[-2], raw = read(x[-1])
+			))
+
+	def _scan_dir(self):
 		# read dir
-		directory = []
-		for r in files:
-			if not r['type'] == DIR:
-				continue
-			_dir = array.array('i', r['raw'])
-			l = 5 if ind.version == 7.1 else 4
-			for i in range(0, len(_dir), l):
-				_r = list(_dir[i : i + l])
-				directory.append(dict( type = _r[0], group = _r[1], instance = _r[2], size = _r[-1]))
-			break # only take 1 dir file
-		for f in files:
-			for d in directory:
-				if match_parts(['type','group','instance'], f, d):
-					f['size'] = d['size']
-		return files
+		dir_records = self.search(tid = DIR)
+		if len(dir_records) != 1:
+			raise DBPFException('incorrect DIR records')
+
+		_dir = array.array('i', dir_records[0].raw)
+		l = 5 if self.index.version == 7.1 else 4
+
+		for i in range(0, len(_dir), l):
+			slice = list(_dir[i : i + l])
+			f = self.search(tid = slice[0], gid = slice[1], iid = slice[2])
+			print f, slice
+			if len(f) != 1:
+				raise DBPFException('multiple instances of TGI')
+			f[0].size = slice[-1]
 
 	def search(self, **kwargs):
-		for f in self.records:
-			if match_parts(kwargs.keys(), kwargs, f):
-				yield f
+		return [f for f in self.records if f == TGI(**kwargs)]
 
 #util
 def version(major, minor): return float('.'.join([str(major),str(minor)]))
 
 #exceptions
-class ArgumentException(Exception):
-	pass
-
-class DBPFException(Exception):
-	pass
+class ArgumentException(Exception): pass
+class DBPFException(Exception): pass
 
 if __name__ == '__main__':
 	import sys
 	db = DBPF(sys.argv[1])
 	def p(*args): print("{:X}:{:X}:{:X}".format(args[0],args[1],args[2]))
-	for f in db.search(type=DIR):
+	print db.version, db.user_version, db.index
+	for f in db.search(tid=DIR):
 		print f['type'], f['group'], f['instance']
