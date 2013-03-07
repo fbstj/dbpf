0ef1655e6ddc8643f3a1799a5fe27a62a02ccbdf
Joe ST <joe@fbstj.net>
2013-03-07 07:39:24 +0000

    [DIR] fix reading - DBPF._table generats slices from array.arrays' corresponding to rows - DBPF._read generates blobs - add docstrings

--- dbpf.py
+++ dbpf.py
@@ -22,7 +22,7 @@ class TGI:
 
 class Index(namedtuple("DBPF_Index", 'version count offset size')): pass
 class Record:
-	def __init__(self, tid, gid, iid, offset, raw=None, size=None):
+	def __init__(self, tid, gid, iid, offset, raw, size=None):
 		self.type = tid
 		self.group = gid
 		self.instance = iid
@@ -42,25 +42,30 @@ class Record:
 		if to.instance is not None:
 			if self.instance != to.instance:
 				return 4
-		print self, "==", to
 		return 0
 	def __repr__(self):
 		return "T{:X}:G{:X}:I{:X}".format(self.type,self.group,self.instance)
 
 class DBPF:
 	@property
-	def version(self): return version(self.header[1], self.header[2])
+	def version(self):
+		"""a real number representing the header version"""
+		return version(self.header[1], self.header[2])
 
 	@property
-	def user_version(self): return version(self.header[3], self.header[4])
+	def user_version(self):
+		"""a real number representing the user version"""
+		return version(self.header[3], self.header[4])
 
 	@property
 	def index(self):
+		"""the table of files in this DBPF"""
 		iv = self.header[8] if self.header[1] == 1 else self.header[15]
 		return Index(iv, self.header[9], self.header[10], self.header[11])
 
 	@property
 	def holes(self):
+		"""the table of holes in this DBPF"""
 		return Index(0, self.header[12], self.header[13], self.header[14])
 
 	def __init__(self, fd):
@@ -76,47 +81,57 @@ class DBPF:
 			raise DBPFException('Not a DBPF file')
 		self.records = []
 		self._scan_records()
-		for f in self.records
-			print f
 		self._scan_dir()
 
-	def _record_bytes(self):
-		return struct.Struct({7.0:'5I', 7.1:'6I'}.get(self.index.version, ''))
+	@property
+	def _index_width(self):
+		"""the width of records in the index table"""
+		return {7.0:5, 7.1:6}.get(self.index.version, '')
 
 	def _scan_records(self):
-		# read index
-		rb = self._record_bytes()
-		o = self.index.offset
-		seek = self._fd.seek
-		read = self._fd.read
-		for id in range(self.index.count):
-			seek(o + rb.size * id)
-			x = rb.unpack(read(rb.size))
-			seek(x[-2])
+		"""parse the records table into self.records"""
+		ind = self.index
+		for rec in self._table(ind.offset, ind.size, self._index_width):
 			self.records.append(Record(
-				tid = x[0], gid = x[1], iid = x[2],
-				offset = x[-2], raw = read(x[-1])
+				tid = rec[0], gid = rec[1], iid = rec[2],
+				offset = rec[-2], raw = rec[-1]
 			))
 
+	@property
+	def _dir_width(self):
+		"""the width of records in the directory table"""
+		return {7.0:4, 7.1:5}.get(self.index.version, '')
+
 	def _scan_dir(self):
-		# read dir
-		dir_records = self.search(tid = DIR)
-		if len(dir_records) != 1:
-			raise DBPFException('incorrect DIR records')
+		"""parse the directory table, appending size variables to appropriate record"""
+		dirs = self.search(tid = DIR)
+		if len(dirs) == 0:
+			return
+		if len(dirs) != 1:
+			raise DBPFException('multiple directory files')
+		d = dirs[0]
+		for rec in self._table(d.offset, d.raw, self._dir_width):
+			res = self.search(tid = rec[0], gid = rec[1], iid = rec[2])
+			res[0].size = rec[-1]
 
-		_dir = array.array('i', dir_records[0].raw)
-		l = 5 if self.index.version == 7.1 else 4
+	def _table(self, offset, length, width):
+		"""parse the passed """
+		self._fd.seek(offset)
+		raw = array.array('L',self._fd.read(length));
+		for i in range(0, len(raw), width):
+			yield list(raw[i : i + width])
 
-		for i in range(0, len(_dir), l):
-			slice = list(_dir[i : i + l])
-			f = self.search(tid = slice[0], gid = slice[1], iid = slice[2])
-			print f, slice
-			if len(f) != 1:
-				raise DBPFException('multiple instances of TGI')
-			f[0].size = slice[-1]
+	def _read(self, tgi=None, **kwargs):
+		"""read the passed file"""
+		if tgi is None: tgi = TGI(**kwargs)
+		for rec in self.search(tgi):
+			self._fd.seek(rec.offset)
+			yield self._fd.read(rec.raw)
 
-	def search(self, **kwargs):
-		return [f for f in self.records if f == TGI(**kwargs)]
+	def search(self, tgi=None, **kwargs):
+		"""search through records for the passed TGI"""
+		if tgi is None: tgi = TGI(**kwargs)
+		return [f for f in self.records if f.__cmp__(tgi) == 0]
 
 #util
 def version(major, minor): return float('.'.join([str(major),str(minor)]))
@@ -130,5 +145,3 @@ if __name__ == '__main__':
 	db = DBPF(sys.argv[1])
 	def p(*args): print("{:X}:{:X}:{:X}".format(args[0],args[1],args[2]))
 	print db.version, db.user_version, db.index
-	for f in db.search(tid=DIR):
-		print f['type'], f['group'], f['instance']
