a33f1fe6b892d7d9f5f87082e4b34fcf58ab8cfe
Joe ST <joe@fbstj.net>
2013-03-08 22:24:13 +0000

    [dbpf] use sqlite as a storage engine - store TGI and base64(file) for each file

--- dbpf.py
+++ dbpf.py
@@ -1,9 +1,32 @@
 import struct
 import array
 from collections import namedtuple
+import sqlite3
+import base64
 
 class Index(namedtuple("DBPF_Index", 'version count offset size')): pass
 class Record(namedtuple("DBPF_Record", 'type group instance offset length size raw')): pass
+
+def ID(value):
+	"""parses valid IDs into integers"""
+	if value is None:
+		return None
+	if isinstance(value, long):
+		return value
+	if isinstance(value, str):
+		return int(value, 16)
+	return None
+
+
+def TGI(tid=None, gid=None, iid=None):
+	where = []
+	if tid is not None:
+		where.append(("tid=?" , ID(tid)))
+	if gid is not None:
+		where.append(("gid=?" , ID(gid)))
+	if iid is not None:
+		where.append(("iid=?" , ID(iid)))
+	return " and ".join([a for a,b in where]), [b for a,b in where]
 
 class DBPF:
 	@property
@@ -26,21 +49,29 @@ class DBPF:
 	def holes(self):
 		"""the table of holes in this DBPF"""
 		return Index(0, self.header[12], self.header[13], self.header[14])
+	
+	def _sql(self, query, args=[]):
+		self._db.execute(query, args)
+		return self._db.fetchall()
 
-	def __init__(self, fd):
+	def __init__(self, fd, db=':memory:'):
 		if isinstance(fd, str):
 			fd = open(fd, 'rb')
 		if not isinstance(fd, file):
 			raise ArgumentException('File')
 		self._fd = fd;
+
 		fd.seek(0)
 		hb = struct.Struct("4s17i24s")
 		self.header = hb.unpack(fd.read(hb.size))
 		if self.header[0] != b'DBPF':
 			raise DBPFException('Not a DBPF file')
+
+		self._db = sqlite3.connect(db).cursor()
+		self._sql("""CREATE TABLE IF NOT EXISTS
+			files ( tid, gid, iid, raw, PRIMARY KEY(tid, gid, iid) )""")
+
 		self._scan_records()
-		self._scan_dir()
-		self._push_raw()
 
 	@property
 	def _index_width(self):
@@ -51,7 +82,14 @@ class DBPF:
 		"""parse the records table into self.records"""
 		ind = self.index
 		for rec in self._table(ind.offset, ind.size, self._index_width):
-			sql.add_index(rec[0], rec[1], rec[2], rec[-2], rec[-1])
+			tgi = TGI(*rec[:3])
+			self._sql("INSERT INTO files(tid, gid, iid) VALUES (?, ? , ?)", tgi[1])
+			self._fd.seek(rec[-2])
+			raw = self._fd.read(rec[-1])
+			args = [base64.encodestring(raw)] + tgi[1]
+			self._sql("UPDATE files SET raw=? WHERE tid=? and gid=? and iid=?", args)
+
+		print "index records scanned"
 
 	@property
 	def _dir_width(self):
@@ -60,7 +98,7 @@ class DBPF:
 
 	def _scan_dir(self):
 		"""parse the directory table, appending size variables to appropriate record"""
-		dirs = sql.search(DIR)
+		dirs = self.search(DIR)
 		if len(dirs) == 0:
 			return
 		if len(dirs) != 1:
@@ -69,11 +107,7 @@ class DBPF:
 		for rec in self._table(d.offset, d.length, self._dir_width):
 			res = sql.search(rec[0], rec[1], rec[2])
 			sql.add_size(rec[0],rec[1],rec[2],rec[-1])
-
-	def _push_raw(self):
-		for rec in self.records:
-			self._fd.seek(rec.offset)
-			sql.add_raw(rec[0],rec[1],rec[2], self._fd.read(rec.length))
+		print "directory records scanned"
 
 	def _table(self, offset, length, width):
 		"""parse the passed """
@@ -84,11 +118,12 @@ class DBPF:
 
 	@property
 	def records(self):
-		sql.c.execute("SELECT * FROM files")
-		return [Record(*rec) for rec in sql.c.fetchall()]
+		return self._sql("SELECT * FROM files")
 
 	def search(self, tid=None, gid=None, iid=None):
-		return [Record(*rec) for rec in sql.search(tid,gid,iid)]
+		where = TGI(tid,gid,iid)
+		query = "SELECT * FROM files WHERE " + where[0]
+		return self._sql(query, where[1])
 
 DIR = 'E86B1EEF'
 EFFDIR = 'EA5118B0'
@@ -104,8 +139,7 @@ class ArgumentException(Exception): pass
 class DBPFException(Exception): pass
 
 if __name__ == '__main__':
-	import sql
 	import sys
 	db = DBPF(sys.argv[1])
-	for r in db.search(3899334383L):
-		print r
+	for r in db.search(tid=0xA5BCF4B):
+		print r[:3]
